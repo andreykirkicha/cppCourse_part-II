70696f79bad9a8a5200396a97bbd66337ccdc326
Andrey Kirkicha <kirkicha.andrey@mail.ru>
2023-08-19 21:05:15 +0300

    Modified "multimethod2" (working but not for testing system)

--- 3.3 (associative containers)/multimethod2.cpp
+++ 3.3 (associative containers)/multimethod2.cpp
@@ -18,9 +18,18 @@ struct Multimethod2
     // Sets implementation of multimethod for classes "class1" and "class2" given by
     // typeid, "func" is a function or functional object which gets two pointers to 'Base' and
     // returns 'Result' value
-    void addImpl(std::type_index const & class1, std::type_index const & class2, FunctionType && func)
+    void addImpl(std::type_index const & class1, std::type_index const & class2, FunctionType func)
     {
-        impl.emplace(std::make_pair(class1, class2), func);
+        std::type_index i1(class1);
+        std::type_index i2(class2);
+
+        if (Commutative) { if (i2 < i1) std::swap(i1, i2); }
+
+        Pair const & key = std::make_pair(i1, i2);
+        auto iterator = impl.find(key);
+
+        if (iterator == impl.end()) { impl.emplace(std::move(key), std::move(func)); }
+        else { iterator->second = std::move(func); }
     }
 
     // Checks if there is implementation of multimethod for
@@ -29,13 +38,12 @@ struct Multimethod2
     {
         std::type_index i1(typeid(*object1));
         std::type_index i2(typeid(*object2));
+
+        if (Commutative) { if (i2 < i1) std::swap(i1, i2); }
+        
         Pair const & key = std::make_pair(i1, i2);
 
-        if (impl.find(key) != impl.end()) return true;
-
-        Pair const & extra_key = std::make_pair(i2, i1);
-
-        return (impl.find(extra_key) != impl.end()) ? true : false;
+        return (impl.find(key) != impl.end()) ? true : false;
     }
 
     // Applies multimethod to the objects using pointers "object1" and "object2"
@@ -44,10 +52,11 @@ struct Multimethod2
         std::type_index i1(typeid(*object1));
         std::type_index i2(typeid(*object2));
 
-        Pair const & key = std::make_pair(i1, i2);
-        FunctionType func = impl.find(key)->second;
+        if (Commutative) { if (i2 < i1) std::swap(i1, i2); }
 
-        return func(object1, object2);
+        Pair const & key = std::make_pair(i1, i2);
+
+        return impl.find(key)->second(object1, object2);
     }
 
 private:
@@ -69,6 +78,8 @@ struct Triangle : Shape {};
 
 bool is_intersect_r_r(Shape * a, Shape * b) { std::cout << "R x R" << std::endl; return true; }
 
+bool is_intersect_rr(Shape * a, Shape * b) { std::cout << "r x r" << std::endl; return true; }
+
 bool is_intersect_r_t(Shape * a, Shape * b) { std::cout << "R x T" << std::endl; return true; }
 
 // ====================================================================================================================
@@ -80,24 +91,20 @@ int main()
     // Commutative
     Multimethod2<Shape, bool, true> is_intersect;
 
-    // Add implementation of multimethod for two rectangles
     is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_r_r);
-
-    // Add implementation of multimethod for rectangle and triangle
     is_intersect.addImpl(typeid(Rectangle), typeid(Triangle), is_intersect_r_t);
 
-    // Make two shapes
     Shape * shape1 = new Triangle();
     Shape * shape2 = new Rectangle();
 
-    // Check if some implementation for "shape1" and "shape2" exists
-    if (is_intersect.hasImpl(shape1, shape2))
-    {
-         // Call is_intersect_r_t(shape2, shape1)
-         bool res = is_intersect.call(shape1, shape2);
+    if (is_intersect.hasImpl(shape1, shape2)) { bool res = is_intersect.call(shape1, shape2); }
+    if (is_intersect.hasImpl(shape2, shape1)) { bool res = is_intersect.call(shape2, shape1); }
 
-         std::cout << res << std::endl;
-    }
+    if (is_intersect.hasImpl(shape1, shape1)) { bool res = is_intersect.call(shape1, shape1); }
+    if (is_intersect.hasImpl(shape2, shape2)) { bool res = is_intersect.call(shape2, shape2); }
+
+    is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_rr);
+    if (is_intersect.hasImpl(shape2, shape2)) { bool res = is_intersect.call(shape2, shape2); }
 
     return 0;
 }
